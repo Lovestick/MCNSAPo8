b2490eae467c1f5ff7e33c0e272109523cc7be9d
Jeremy Randolph <jeremy.randolph@gmail.com>
2013-03-21 13:42:06 -0500

    Unused import.

--- src/com/aegamesi/mc/po8/Po8Plugin.java
+++ src/com/aegamesi/mc/po8/Po8Plugin.java
@@ -6,7 +6,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
-import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
